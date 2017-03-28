ee7decdc109f4040a6a727665c7c4e6f95558deb
CHRIS <chris.shandgost@gmail.com>
2017-03-29 10:58:09 +1100

    Fixed

---
+++ tree_search/src/Timer.cpp
@@ -0,0 +1,26 @@
+#include "../include/Timer.h"
+
+ChronoTimer::ChronoTimer()
+{
+
+}
+
+ChronoTimer::~ChronoTimer()
+{
+
+}
+
+void ChronoTimer::StartTimer()
+{
+	_startTime = std::chrono::high_resolution_clock::now();
+}
+
+void ChronoTimer::EndTimer()
+{
+	_endTime = std::chrono::high_resolution_clock::now();
+}
+
+std::string ChronoTimer::PrintTime_ms()
+{
+	return (std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(_endTime - _startTime).count()) + "ms");
+}
